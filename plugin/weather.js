a96c0e33e9f3ec1a94a7b9eb8e4db0bc0783c75a
rahulbotmaster1 <rahulbotmaster00@gmail.com>
2026-02-18 22:36:53 +0530

    Update weather.js

--- plugin/weather.js
+++ plugin/weather.js
@@ -30,7 +30,7 @@ async (conn, mek, m, { from, q, reply }) => {
 💨 *Wind Speed*: ${data.wind.speed} m/s
 🔽 *Pressure*: ${data.main.pressure} hPa
 
-*POWERED BY LITE-CLD*
+*POWERED BY CYBER-MD*
 `;
         return reply(weather);
     } catch (e) {
