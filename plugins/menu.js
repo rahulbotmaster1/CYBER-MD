8c0a670572461480cce9b3c3129993386ebbcbf1
rahulbotmaster1 <rahulbotmaster00@gmail.com>
2026-02-21 21:59:28 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -2,7 +2,7 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
 cmd({
-    pattern: "menu2",
+    pattern: "menu",
     react: "🌠",
     desc: "get cmd list",
     category: "main",
