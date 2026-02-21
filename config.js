6b5f6d959c6ebec0207960450a210a80f8bbb4cf
rahulbotmaster1 <rahulbotmaster00@gmail.com>
2026-02-21 12:05:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,6 +12,6 @@ SUDO_NB: process.env.SUDO_NB || "919356730236",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE:"true"
-process.env.AUTO_REPLY || "true"
+AUTO_REPLY: process.env.AUTO_REPLY || "true",
 };
 
