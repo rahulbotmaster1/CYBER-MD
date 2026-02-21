773fe1787dbe37863a4bcbc543013876820261e5
rahulbotmaster1 <rahulbotmaster00@gmail.com>
2026-02-21 14:39:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "", //Put your session code here
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/5xGNHz7Q/temp.jpg",ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/nN2rP4PP/temp.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "*HEY DEAR* ${pushname}\n *I Am *CYBER-MD* Alive Now ✔️*",
-SUDO_NB: process.env.SUDO_NB || "919356730236",
+SUDO_NB: process.env.SUDO_NB || "918149730236",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE:"true"
