be152486161bb963ada718201441a775daea1de8
rahulbotmaster1 <rahulbotmaster00@gmail.com>
2026-02-21 22:04:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //Put your session code here
+SESSION_ID: process.env.SESSION_ID || "MrgE1bxQ#QXcv1VGsjvT-lfQNvPxq4vLHmQDP1LTOvcg8G7NLB24", //Put your session code here
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/5xGNHz7Q/temp.jpg",ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/nN2rP4PP/temp.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "*HEY DEAR* ${pushname}\n *I Am *CYBER-MD* Alive Now ✔️*",
 SUDO_NB: process.env.SUDO_NB || "918149730236",
