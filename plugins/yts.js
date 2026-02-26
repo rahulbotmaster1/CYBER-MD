62a877864ffc8b5cf6fc56d2e623f628b817d226
rahulbotmaster1 <rahulbotmaster00@gmail.com>
2026-02-26 15:34:42 +0530

    Update yts.js

--- plugins/yts.js
+++ plugins/yts.js
@@ -11,7 +11,7 @@ const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, 
 cmd({
     pattern: "yts",
     alias: ["ytsearch"],
-    use: '.yts Ryan xmd bot',
+    use: '.yts CYBER-MD bot',
     react: "🔎",
     desc: "Search and get details from youtube.",
     category: "search",
